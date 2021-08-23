8ff5517e9c2b567d0ced04a6a0b038793e7eaffd
Dávid Urbán <david.urban@icellmobilsoft.hu>
2021-08-23 16:19:37 +0200

    Added Brick

--- CannonBall.gd
+++ CannonBall.gd
@@ -1,11 +1,15 @@
 extends RigidBody
 
-export var muzzle_velocity = 30.0
+export var muzzle_velocity = 30
 export var g = Vector3.DOWN * 10
+
+var shooted = false
 
 var velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	velocity += g * delta
-	apply_central_impulse(transform.origin + velocity.normalized())
-	transform.origin += velocity * delta
+	if (!shooted):
+		velocity += g * delta
+		apply_central_impulse(velocity.normalized() * muzzle_velocity)
+		transform.origin += velocity * delta
+		shooted = true
